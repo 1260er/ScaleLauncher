1e5253f2e8e63ceeec7e8babd94ec775bf97b3d7
1260er <app-login@mail.de>
2026-09-11 11:28:38 +0200

    Start 1.7.0 development

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "de.pritcloud.scalelauncher"
         minSdk = 31
         targetSdk = 35
-        versionCode = providers.environmentVariable("SCALELAUNCHER_VERSION_CODE").orNull?.toIntOrNull() ?: 8
-        versionName = "1.6.0"
+        versionCode = providers.environmentVariable("SCALELAUNCHER_VERSION_CODE").orNull?.toIntOrNull() ?: 9
+        versionName = "1.7.0"
 
         javaCompileOptions {
             annotationProcessorOptions {
